9a1a469918bdda88f9d8e7d536216209b3b932f5
Yashraj Mishra <yashrajmishra067@gmail.com>
2026-03-17 04:37:35 +0530

    Add PORT

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,9 @@ mongoose.connect(config.connectionString)
 .then(() => {
     console.log("MongoDB connected");
 
-    app.listen(8000, () => {
-        console.log("Server running on port 8000");
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
     });
 })
 .catch(err => {
